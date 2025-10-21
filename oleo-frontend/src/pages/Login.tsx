52e64736a79ec05048dd7183c5c6add1cf02c6d5
vandson silva <vandsonsilva@bokus.com.br>
2025-10-21 15:14:12 -0300

    adicionando navigate

--- oleo-frontend/src/pages/Login.tsx
+++ oleo-frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../contexto/AuthContext";
+import {useNavigate} from 'react-router-dom'
 import api from "../services/api";
 import "./Login.css"
 
@@ -8,6 +9,8 @@ export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -42,9 +45,9 @@ export const Login = () => {
 
       // redireciona conforme o cargo
       if (user.role === "ADMIN") {
-        window.location.href = "/";
+        navigate("/");
       } else {
-        window.location.href = "/";
+        navigate("/");
       }
     } catch (err: any) {
       console.error(err);
